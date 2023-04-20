9950e8e939c9746070108b6d50f4915c4d830fe8
Stav Dahan <stav0405@gmail.com>
2023-04-20 16:32:14 +0300

    created user schecma

--- backend/model/goalModel.js
+++ backend/model/goalModel.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const goalShecma = mongoose.Schema({
+const goalSchema = mongoose.Schema({
     text: {
         type: String,
         required: [true, 'Please add a text value'],
     },
 }, {
     timestamps: true,
-})
+});
 
-module.exports = mongoose.model('Goal', goalShecma)
+module.exports = mongoose.model('Goal', goalSchema)
